3bc3d5e859f523c91e3fa53313523a8c45df00f3
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-29 15:49:57 +0000

    Updated test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,11 +1,11 @@
-describe('FizzBuzz' function() {
+describe('FizzBuzz', function() {
     var fizzBuzz;
 
     beforeEach(function() {
     fizzBuzz = new FizzBuzz();
     });
 
-    it('if number divisble by 3, return fizz' function() {
+    it('if number divisble by 3, return fizz', function() {
       expect(fizzBuzz(3)).toEqual('Fizz');
     });
 
