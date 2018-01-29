deecba72853fcab70fd83dcfdd9638e1f3d1aaeb
samuelmaker <samuelwood.co@gmail.com>
2018-01-29 15:54:31 +0000

    fizzbuzz tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -9,4 +9,16 @@ describe('FizzBuzz', function() {
       expect(fizzBuzz(3)).toEqual('Fizz');
     });
 
+    it('if number divisble by 5, return buzz', function() {
+      expect(fizzBuzz(5)).toEqual('Buzz');
+    });
+
+    it('if passed 7, will return 7', function() {
+      expect(fizzBuzz(7)).toEqual(7);
+    });
+
+    it('if passed 15, will return FizzBuzz', function() {
+      expect(fizzBuzz(15)).toEqual('FizzBuzz');
+    });
+
 });
