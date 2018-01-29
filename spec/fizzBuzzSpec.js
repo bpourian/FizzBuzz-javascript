89c2365a0423ae56dbf98d7f9428ac2971cb15a6
samuelmaker <samuelwood.co@gmail.com>
2018-01-29 15:45:33 +0000

    jasmine setup

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,1 +1,12 @@
-test
+describe('FizzBuzz' function() {
+    var fizzBuzz;
+
+    beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+    });
+
+    it('if number divisble by 3, return fizz' function() {
+      expect(fizzBuzz(3)).toEqual('Fizz');
+    });
+
+});
